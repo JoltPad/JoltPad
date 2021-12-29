555de6978c91d7ae5d456fa8aeadb215a80396a9
Josh Jordan <josh.r.jordan@gmail.com>
2021-12-29 14:49:18 -0500

    fixed connection

--- client/components/DailyCard.jsx
+++ client/components/DailyCard.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 
 //dailyCard renders a card, card is the oject from the array
 const DailyCard = (props) => {
   
   const { notes, index } = props;
   
-  
   return (
     <>
       <div className="notecard">
         <div className="content-container">
           <div className="contents">
-            {/* {notes[index].note_id} */}
+            {notes[index].contents}
           </div>
           <div className="category-name">
-            
+          
           </div>
         </div>
       </div>
--- client/containers/DailyContainer.jsx
+++ client/containers/DailyContainer.jsx
@@ -3,31 +3,30 @@ import DailyCard from '../components/DailyCard.jsx';
 
 const DailyContainer = (props) => {
   
-  // const data = [
-  //   { contents: 'practice yoga', category_name: 'To-Do' }, 
-  //   { contents: 'buy champagne', category_name: 'Shopping' }, 
-  //   { contents: 'When will the NeuralLink be my reality?', category_name: 'Thoughts' },
-  //   { contents: 'order ninja stars', category_name: 'Shopping' }, 
-  //   { contents: 'build the sauna', category_name: 'To-Do' }, 
-  //   { contents: 'Change is the only constant', category_name: 'Thoughts' }
-  // ]
+  const data = [
+    { contents: 'practice yoga', category_name: 'To-Do' }, 
+    { contents: 'buy champagne', category_name: 'Shopping' }, 
+    { contents: 'When will the NeuralLink be my reality?', category_name: 'Thoughts' },
+    { contents: 'order ninja stars', category_name: 'Shopping' }, 
+    { contents: 'build the sauna', category_name: 'To-Do' }, 
+    { contents: 'Change is the only constant', category_name: 'Thoughts' }
+  ]
   //declare state
-  const [ notes, setNotes ] = useState([]);
+  const [ notes, setNotes ] = useState([data]);
   const [ index, setIndex ] = useState(0);
   
-  const user_id = 2;
+
   // //send fetch request to get daily notes from user_id 
   useEffect(() => {
 
     const getNotes = async () => {
       
       const notesFromDB = await fetchNotes();
+      
       setNotes(notesFromDB);
-      console.log('notes from inside getNotes', notes)
+      
     }
-
     getNotes();
-    
   }, [])
   
   //send update note
@@ -54,12 +53,13 @@ const DailyContainer = (props) => {
   } 
 
   const fetchNotes = async () => {
-    const result = await fetch(`http://localhost:3000/daily/2`);
-    const data = await result.json();
-    console.log(data);
+    const user_id = localStorage.userID;
+    const res = await fetch(`http://localhost:3000/daily/${user_id}`);
+    const data = await res.json();
+    console.log('on load', data);
     return data;
   }
-
+  console.log(notes);
   return(
     <div className='dailyContainer'>
       <h3>Your Daily Cards</h3>
@@ -67,7 +67,6 @@ const DailyContainer = (props) => {
       <div className="btn-container" >
         <button className="btn" onClick={handlePrev}>Previous</button>
         <button className="btn" onClick={handleNext}>Next</button>
-        {/* <button className="btn" onClick={((e) => fetchNotes(2))}>Get Cards</button> */}
       </div>
     </div>
   )
