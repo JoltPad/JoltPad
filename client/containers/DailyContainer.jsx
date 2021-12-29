4bfa3247b90ae248d777075fc2587e6c442782f9
Josh Jordan <josh.r.jordan@gmail.com>
2021-12-28 23:00:32 -0500

    daily card and container progress

---
+++ client/containers/DailyContainer.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import DailyCard from '../components/DailyCard.jsx';
+
+
+const DailyContainer = (props) => {
+  
+  const data = [
+    { contents: 'practice yoga', category_name: 'To-Do' }, 
+    { contents: 'buy champagne', category_name: 'Shopping' }, 
+    { contents: 'When will the NeuralLink be my reality?', category_name: 'Thoughts' },
+    { contents: 'order ninja stars', category_name: 'Shopping' }, 
+    { contents: 'build the sauna', category_name: 'To-Do' }, 
+    { contents: 'Change is the only constant', category_name: 'Thoughts' }
+  ]
+  //declare state
+  const [ notes, setNotes ] = useState(data);
+  const [ index, setIndex ] = useState(0);
+  
+ 
+  // //send fetch request to get daily notes from user_id 
+  // useEffect(() => {
+
+  //   fetch(`http://localhost:3000/daily/${user_id}`)
+  //   .then((response) => response.json())
+  //   .then((data) => setNotes(data))
+  //   .catch(err => console.log(err))
+  // })
+
+  //send update note
+  const updateNote = () => {
+    fetch(`http://localhost:3000/updateNote/`)
+
+  }
+
+  //send delete note
+  const deleteNote = () => {
+    fetch(`http://localhost:3000/deleteNote/`)
+  } 
+
+  const handleNext = () => {
+    event.preventDefault();
+    //if the current index is the last element in the array, return to the beginning 
+    setIndex(index === notes.length - 1 ? 0 : index + 1);
+  } 
+
+  const handlePrev = () => {
+    event.preventDefault();
+    //if the current element is the first element, go backwards to the last element
+    setIndex(index === 0 ? notes.length - 1 : index - 1);
+  } 
+  return(
+    <>
+      <h3>Your Daily Cards</h3>
+      <DailyCard className="daily-card" notes={notes} index={index} />
+      <div className="btn-container" >
+        <Button className="btn" onClick={handleNext}>Next</Button>
+        <Button className="btn" onClick={handlePrev}>Previous</Button>
+      </div>
+    </>
+  )
+}
+
+export default DailyContainer;
