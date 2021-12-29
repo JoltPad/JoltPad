59ec0fbb69d9c2e6744fe4859c74c8ff51891175
Adam Berri <adamberri123@gmail.com>
2021-12-29 14:03:23 -0500

    drag and drop working

--- client/components/HoverDiv.jsx
+++ client/components/HoverDiv.jsx
@@ -1,12 +1,47 @@
-import * as React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 
 
 export default function HoverDiv(props) {
 
+const [style, setStyle] = useState({})
+function isHover () {
+  if (props.hoverOn) {
+    return {backgroundColor: props.colorProps}
+  }
+}
+const handleDragEnter = e => {
+  e.preventDefault();
+  e.stopPropagation();
+};
+const handleDragLeave = e => {
+  e.preventDefault();
+  e.stopPropagation();
+};
+const handleDragOver = e => {
+  e.preventDefault();
+  e.stopPropagation();
+};
+const handleDrop = e => {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log('dropped');
+  console.log(props.className)
+};
+
+useEffect(() => {
+  setStyle(isHover());
+  console.log(style);
+}, [props.hoverOn]);
+
   return (
-    <div className={props.className} style={{backgroundColor: props.colorProps}}> This is a HoverDiv component</div>
+    <div className={props.className} 
+      onDrop={e => handleDrop(e)}
+      onDragOver={e => handleDragOver(e)}
+      onDragEnter={e => handleDragEnter(e)}
+      onDragLeave={e => handleDragLeave(e)}    
+      style={style}> 
+    </div>
   )
 
 }
