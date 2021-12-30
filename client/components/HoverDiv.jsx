e3918e3a6098a892357d3360c1f1a2b08f197918
Adam Berri <adamberri123@gmail.com>
2021-12-29 19:06:15 -0500

    blurs

--- client/components/HoverDiv.jsx
+++ client/components/HoverDiv.jsx
@@ -21,11 +21,20 @@ export default function HoverDiv(props) {
       return categoryText[a]
     } 
   }
+  // function setColor(color) {
+  //   const obj = {
+  //     box-shadow: `inset 0 0 50px ${color},inset 20px 0 80px ${color},inset - 20px 0 80px ${color},inset 20px 0 300px ${color},inset - 20px 0 300px ${color},0 0 20px ${color},-10px 0 40px ${color},10px 0 40px ${color};`
+  
+  //   }
+  //   console.log(color)
+  //   return {box}
+  // }
 
   function isHover() {
     if (props.hoverOn) {
-
-      return { backgroundColor: props.colorProps }
+      const myMessage = 'inset 0 0 50px #fff, inset 20px 0 80px #f0f, inset -20px 0 80px #0ff, inset 20px 0 300px #f0f, inset -20px 0 300px #0ff, 0 0 20px #fff, -10px 0 40px #f0f, 10px 0 40px #0ff';
+      const newMessage = myMessage.replace(/#f0f/g, props.colorProps);
+      return {boxShadow: newMessage}
     }
   }
   const handleDragEnter = e => {
@@ -49,7 +58,7 @@ export default function HoverDiv(props) {
     const cardText = document.querySelector('#card-input').value
     document.querySelector('#card-input').value = ''
 
-    const params = { user_id: localStorage.userID, category_id: props.categoryID, contents: cardText };
+    const params = { user_id: localStorage.userID, category_id: props.categoryID, contents: props.noteInput };
     
     axios
       .post("/add", params)
@@ -63,6 +72,7 @@ export default function HoverDiv(props) {
 
   useEffect(() => {
     setStyle(isHover());
+    console.log(style)
     setInsideText(divText(props.categoryID));
   }, [props.hoverOn]);
 
